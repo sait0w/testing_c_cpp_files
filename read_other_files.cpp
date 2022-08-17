1f7d6fddf75bbbfd99bec98bccdcdf524aa62ab8
Pedro Saito <phenriquesaito@gmail.com>
2022-08-17 16:30:50 -0300

    alterations_to_readfiles

--- read_other_files.cpp
+++ read_other_files.cpp
@@ -3,19 +3,28 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main(){
  int curs;
+ char contTexto[100];
+ system("clear");
 
  FILE *file;
- file = fopen("texto.txt","r");
+ file = fopen("README.md","r");
+ int cont;
  if(file){
   while((curs = getc(file)) != EOF){
     printf("%c", curs);
+    contTexto[cont] = curs;
+    cont++;
   }
  }
  fclose(file);
-
+ //Imprimindo conteúdo lido
+ for(cont=0;cont<strlen(contTexto);cont++){
+  printf("%c", contTexto[cont]);
+ }  
  return 0;
 }
