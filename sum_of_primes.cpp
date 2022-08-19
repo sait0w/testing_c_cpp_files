b6b27d13d482688ec31d17d9f4b767a3e10d9c27
Pedro Saito <phenriquesaito@gmail.com>
2022-08-19 00:56:34 -0300

    exercises_usp_c

---
+++ sum_of_primes.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+ int lim, np, cont, sum = 0, aux = 0, rs;
+
+ printf("Digite um valor limite:");
+ scanf("%d", &lim);
+  for(np=1;np<=lim;np++){
+    aux = 0;
+    for(cont=1;cont<=np;cont++){
+     if(np%cont == 0){
+      aux++; //aux == 1;
+     }
+     
+    }
+    if(aux == 2){
+      sum+=np;
+     }
+  }
+  printf("\nA soma é: %d", sum);
+
+ return 0;
+}
