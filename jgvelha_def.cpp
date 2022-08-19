2701a9c55b42107ae4c3e18930efcf5262a3c997
Pedro Saito <phenriquesaito@gmail.com>
2022-08-19 16:20:01 -0300

    jgvelha_changes

--- jgvelha_def.cpp
+++ jgvelha_def.cpp
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <iostream>
-#define nl 5
 
 //Calling functions at the end, after main.
-int clear();
+void clear();
 void jgvelha();
 void csfunction(int i, int j,int cvtr[3][3]);
+void trerow(int v[11]);
 
 int main(){
  clear();       
  int  i, j, mnselect;
  int cvtr[3][3];
  int v[11];
- int random = 144;
+ int random = 4;
  setlocale(LC_ALL,"");
  
  do{
@@ -27,52 +27,36 @@ int main(){
  scanf("%d", &mnselect);
  clear();
  }while(mnselect!=1); //Set mnselect value to 1 in order to exit the menu.
-
+ // x---------------x
  //Reset every value of cvtr[i][j] to random integer >= 0.
- for(i=0;i<3;i++){       //<---- Verify if this is right!!
+ for(i=0;i<3;i++){      
   for(j=0;j<3;j++){
     cvtr[i][j] = random;
     random++; 
   }
-  random = random % 17;
  }
+ random = 4;
+ // x--------------x
+ for(mnselect=1;mnselect<10;mnselect++){
+   v[mnselect] = random;
+   random++;
+  }
+ mnselect = 1;
  jgvelha();
- do{          //Beggining of "do_while" type of repetition.
  v[10] = 1;
+ do{          //Beggining of "do_while" type of repetition.
  printf("\n Selecione _posição_ + _caractere escolhido_:");
  scanf("%d %d", &i, &j);        //in the matrix to the coordenates choosen.
  csfunction(i, j, cvtr);
+ mnselect = 1;
   for(i=0;i<3;i++){     //"mnselect" = 1 i.e. vector begins in 1 instead of 0.
    for(j=0;j<3;j++){ 
    v[mnselect] = cvtr[i][j];        //<---- 'Error'.
    mnselect++; 
    }
  }
- mnselect = 1;
- if(v[1] == v[2] && v[3]){
-   v[10] = 0;
- }
- if(v[1] == v[4] && v[7]){
-   v[10] = 0;
- }
- if(v[1] == v[5] && v[9]){
-   v[10] = 0;
- }
- if(v[4] == v[5] && v[6]){
-   v[10] = 0;
- }
- if(v[7] == v[8] && v[9]){
-   v[10] = 0;
- }
- if(v[3] == v[5] && v[7]){
-   v[10] = 0;
- }
- if(v[2] == v[5] && v[8]){
-   v[10] = 0;
- }
- if(v[3] == v[6] && v[9]){
-   v[10] = 0;
- }
+ v[10] = 1;
+ trerow(v);
  }while(v[10] == 1);
  return 0;
 }
@@ -85,9 +69,8 @@ void jgvelha(){          //UI of "jogo da velha".
  std::cout <<" 2,0 | 2,1 | 2,2\n";
 }
 
-int clear(){
+void clear(){
  system("clear");
- return 1;
 }
 
 void csfunction(int i,int j,int cvtr[3][3]){   //"Choosing function" attributes a position 
@@ -101,4 +84,31 @@ void csfunction(int i,int j,int cvtr[3][3]){   //"Choosing function" attributes 
  case 4:
   scanf("%d", &cvtr[2][2]);
  } 
+}
+
+void trerow(int v[11]){
+	if(v[1] == v[2] && v[2] == v[3]){
+   v[10]++;
+ }else
+ if(v[1] == v[4] && v[4] == v[7]){
+   v[10]++;
+ }else
+ if(v[1] == v[5] && v[5] == v[9]){
+   v[10]++;
+ }else
+ if(v[4] == v[5] && v[5] == v[6]){
+   v[10]++;
+ }else
+ if(v[7] == v[8] && v[8] == v[9]){
+   v[10]++;
+ }else
+ if(v[3] == v[5] && v[5] == v[7]){
+   v[10]++;
+ }else
+ if(v[2] == v[5] && v[5] == v[8]){
+   v[10]++;
+ }else
+ if(v[3] == v[6] && v[6] == v[9]){
+   v[10]++;
+ }
 }
