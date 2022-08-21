7535610aa7647aa35526d417100cd4d004ed9ed0
Pedro Saito <phenriquesaito@gmail.com>
2022-08-21 00:05:16 -0300

    more_piles

--- exercises.cpp
+++ exercises.cpp
@@ -1,73 +1,130 @@
-#include <stdio.h>
 #include <iostream>
-#include <new>
-#include <string>
 #include <stdlib.h>
+#include <string>
 #define TAM 10
  
-using namespace std; //para utilizar cout
+using namespace std;
  
-void imprime_vetor(int vetor[TAM]){
+void imprime_vetor(int vetor[TAM], int topo){
  
     //Auxiliar contador
     int cont;
+ 
+    cout << "\n";
  
      //Imprime o vetor
     for(cont = 0; cont < TAM; cont++){
         cout << vetor[cont] << " - ";
     }
  
+    cout << "Topo: " << topo;
+ 
 }
  
-int busca_simples(int vetor[TAM], int valorProcurado, int *posicaoEncontrada,int i = 20){
+//Caso a pilha esteja vazia
+bool pilha_vazia(int topo){
+    if(topo == -1){
+        return true;
+    }else{
+        return false;
+    }
+}
+ 
+//Confere se a pilha está cheia
+bool pilha_cheia(int topo){
+     if(topo == TAM - 1){
+        return true;
+    }else{
+        return false;
+    }
+}
+ 
+ 
+int pilha_tamanho(int topo){
+    return topo + 1;
+}
+ 
+//Retorna o ultimo valor da pilha
+int pilha_get(int pilha[TAM], int *topo){
+    if(pilha_vazia(*topo)){
+        cout << "A pilha esta vazia";
+        return 0;
+    }else{
+        return pilha[*topo];
+    }
+}
+ 
+//Adiciona um valor na pilha
+void pilha_push(int pilha[TAM],int valor, int *topo){
+ 
+    //Caso não possa colocar mais valores
+    if(pilha_cheia(*topo)){
+        cout << "Pilha cheia!";
+    }else{
+        *topo = *topo + 1;
+        pilha[*topo] = valor;
+    }
+ 
+}
+ 
+//Remove um valor da pilha
+void pilha_pop(int pilha[TAM], int *topo){
+ 
+    if(pilha_vazia(*topo)){
+        cout << "A Pilha ja esta vazia";
+    }else{
+        cout << "Valor Removido:" << pilha[*topo];
+        pilha[*topo] = 0;
+        *topo = *topo -1;
+    }
+ 
+}
+ 
+//Cria uma pilha e limpa ela
+void pilha_construtor(int pilha[TAM], int *topo){
+ 
+    //Coloca o topo negativo para indicar uma pilha vazia
+    *topo = -1;
  
     //Auxiliar contador
     int cont;
  
-    //Verifica se o valor foi encontrado
-    bool valorFoiEncontrado;
- 
-    //Percorre a lista em busca do valor
+     //Imprime o vetor
     for(cont = 0; cont < TAM; cont++){
-        if(vetor[cont] == valorProcurado){
-            valorFoiEncontrado = true;
-            *posicaoEncontrada = cont;
-        }
+       pilha[cont] = 0;
     }
  
-    if(valorFoiEncontrado){
-        return 1;
-    }else{
-        return -1;
-    }
-
 }
+ 
+ 
  
 int main(){
  
-    int vetor[TAM] = {1,23,44,56,63,72,84,90,98};
-    int valorProcurado;
-    int posicao, posicaoEncontrada;
-    int i;
+    int pilha[TAM];
+    int topo, valorRetirado ;                                 //Topo da pilha
  
-    //Auxiliar contador
-    int cont;
+    pilha_construtor(pilha, &topo);
  
-    imprime_vetor(vetor);
+    imprime_vetor(pilha, topo);
  
+    pilha_push(pilha, 5, &topo);
+    imprime_vetor(pilha, topo);
  
-    printf("Qual numero deseja encontrar?");
-    scanf("%d", &valorProcurado);
+    cout << "Ultimo valor da pilha" << pilha_get(pilha, &topo);
+    cout << "Tamanho da Pilha:" << pilha_tamanho(topo);
  
-    if(busca_simples(vetor, valorProcurado, &posicaoEncontrada, i) == 1){
-        cout << "O valor foi encontrado na posicao:\n" << posicaoEncontrada;
-    }else{
-        cout << "Valor nao encontrado\n";
-    }
-    for(i=1;i<TAM;i++){
-     printf("Vetor[%d] = %d\n", i, vetor[i]);
-    }
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 7, &topo);
+    imprime_vetor(pilha, topo);
  
-    return 0;
+    pilha_pop(pilha, &topo);
+    imprime_vetor(pilha, topo);
  
 }
--- pile_1.cpp
+++ pile_1.cpp
@@ -9,38 +9,67 @@ void cls(){
  system("clear");
 }
 
-void print_vector(int vetor[tam]){
- int cont;
-  for(cont = 0; cont < tam; cont++){
- cout << vetor[cont] << " - ";
- }
- 
+int pile_size(int topo){
+ return ++topo;
 }
 
-void pile_push(int pilha[tam],int *topo, int random){
- if(*topo == tam - 1){
-  cout << "Pilha cheia";
- }else{
-  *topo+=1;
-  pilha[*topo] = random;
- 
- }
-}
-
-bool pile_empty(int *topo){
- if(*topo == -1){
+bool pile_empty(int topo){
+ if(topo == -1){
   return true;
  }else{
   return false;
  }
 }
 
+bool pile_full(int topo){
+ if(topo == tam - 1){    //Valor limite do 'topo' é 9.
+  return true;
+ }else{
+  return false;
+ }
+}
+
+void print_vector(int vetor[tam]){
+ int cont;
+  for(cont = 0; cont < tam; cont++){
+ cout << vetor[cont] << " - ";
+ }
+}
+
+void pile_push(int pilha[tam],int *topo, int random){
+ if(pile_full(*topo)){     //'Topo' não delimita o armazenamento do vetor. 
+  cout << "Pilha cheia";   //É dito fictício.
+ }else{
+  *topo+=1;
+  pilha[*topo] = random;
+ }
+}
+
 void pile_pop(int pilha[tam], int *topo){
- if(*topo == -1){
-  cout << "A pilha está vazia.";
+ if(pile_empty(*topo)){
+  cout << "Pilha vazia..";
  }else{
   cout << "Valor removido: " << pilha[*topo];
+  pilha[*topo] = 0; 
   *topo-=1;
+ }
+}
+
+void pile_build(int pilha[tam], int *topo){
+ *topo = -1;
+ int cont;
+  for(cont=0;cont<tam;cont++){
+    pilha[cont] = 0;
+  }
+
+}
+
+int pile_get(int pilha[tam], int *topo){
+ if(pile_empty(*topo)){
+   printf("A pilha está vazia..");
+  return 0;
+ }else{
+  return pilha[*topo];
  }
 }
 
@@ -48,17 +77,12 @@ int main(){
  cls();
  int pilha[tam];
  int topo = -1;  //Definindo o limite da pilha.
- int random, i;
-  for(i=0;i<tam;i++){
- pilha[i] = 0;  
- }
+ int random;
+ 
+ pile_build(pilha, &topo);
+ cout << "Tamanho da pilha: " << pile_size(topo) << "\n";
+  print_vector(pilha);
  scanf("%d", &random);
-
- print_vector(pilha);
-  if(pile_empty(&topo)){
-   cout << "Pilha vazia..\n";
-  }
-
  pile_push(pilha, &topo, random);
  pile_push(pilha, &topo, random);
  pile_push(pilha, &topo, random);
@@ -68,9 +92,19 @@ int main(){
  pile_push(pilha, &topo, random);
  pile_push(pilha, &topo, random);
  pile_push(pilha, &topo, random);
+ pile_push(pilha, &topo, random);
+ pile_push(pilha, &topo, random);
+ cout << "Topo: " << topo << "\n";
+ cout << "ultimo valor da pilha: " << pile_get(&topo, pilha);
+ cout << "\n";
  print_vector(pilha);
  pile_pop(pilha, &topo);
- 
+ cout << "\nTamanho da pilha: " << pile_size(topo) << "\n";
+ pile_build(&topo, pilha);
+ cout << "\n";
+ print_vector(pilha);
+ cout << "Tamanho da pilha: " << pile_size(topo) << "\n";
+
 
  return 0;
 }
