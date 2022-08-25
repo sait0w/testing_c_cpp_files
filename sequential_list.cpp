08d7c5c33930ca2565f4acc337ee400ff700228d
Pedro Saito <phenriquesaito@gmail.com>
2022-08-25 16:26:42 -0300

    sequential_list

--- sequential_list.cpp
+++ sequential_list.cpp
@@ -3,10 +3,7 @@
 #include <string>
 #include <string.h>
 #include <stdlib.h>
- 
 using namespace std; 
-
-
 
 typedef struct pessoa{
     string nome;
@@ -17,23 +14,94 @@ void cls(){
  system("clear");
 }
 
-void print_vector(pessoa *identity,int size){
+void addinit(pessoa *&pointer,int *size,string nome,int rg){
+ if(*size == 0){
+ pessoa *novalist = new pessoa[++*size];
+ novalist[0].nome = nome;
+ novalist[0].rg = rg;
+
+ pointer = novalist;
+ }else{
+
+ pessoa *novalist = new pessoa[++*size];
+ novalist[0].nome = nome;
+ novalist[0].rg = rg;
+ int cont;
+ for(cont=0;cont<*size;cont++){
+  novalist[cont + 1].nome = pointer[cont].nome;
+  novalist[cont + 1].rg = pointer[cont].rg;
+  pointer = novalist;
+ } 
+ ++*size;
+ }
+}
+
+void addend(pessoa *&pointer,int *size,string nome,int rg){
+ pessoa *novalist = new pessoa[++*size];
+ int cont;
+ for(cont=0;cont<*size;cont++){
+  novalist[cont].nome = pointer[cont].nome;
+  novalist[cont].rg = pointer[cont].rg;
+ }
+ novalist[*size].nome = nome;
+ novalist[*size].rg = rg;
+
+ pointer = novalist;
+ ++*size;
+}
+
+void print_vector(pessoa *pointer,int size){
  int cont;
   for(cont=0;cont<size;cont++){
-   cout << cont << "- " << identity[cont].nome << "," << identity[cont].rg << "\n";
+   cout << cont << "- " << pointer[cont].nome << "," << pointer[cont].rg << "\n";
   }
 }
 
 int main(){
- int size;
+ int size = 0, iput = 1;
  struct info;
- pessoa *identity = new pessoa[2];
+ pessoa *pointer;
 
- identity[0].nome = "Pedro";
- identity[0].rg = 31580177;
- identity[1].nome = "Joao";
- identity[1].rg = 32112621;
- print_vector(identity, 2);
+ while(iput < 10 && iput > 0){
+  print_vector(pointer, size);
+        cout << "Tamanho Atual[" << size << "]\n";
+        cout << "Operacoes \n";
+        cout << "1 - Insercao de um node no inicio da lista \n";
+        cout << "2 - Insercao de um node no fim da lista \n";
+        cout << "3 - Insercao de um node na posicao N \n";
+        cout << "4 - Retirar um node do inicio da lista \n";
+        cout << "5 - Retirar um node no fim da lista \n";
+        cout << "6 - Retirar um node na posicao N \n";
+        cout << "7 - Procurar um node com o campo RG \n";
+        cout << "8 - Imprimir a Lista. \n";
+        cout << "9 - Sair do sistema. \n";
+        cout << "\nEscolha um numero e pressione ENTER: \n";
+  cin >> iput;
+  cls();
+  string nome;
+  int rg;
 
+  switch(iput){
+   case 1:
+  cout << "Funcao escolhida: 1 - Insercao de um node no inicio da lista \n";
+  cout << "Digite um nome:";
+  cin >> nome;
+  cout << "Digite um rg:";
+  cin >> rg;
+  addinit(pointer, &size, nome, rg);
+   break;
+   case 2:
+  cout << "Funcao escolhida: 2 - Insercao de um node no fim da lista \n";
+  cout << "Digite um nome:";
+  cin >> nome;
+  cout << "Digite um rg:";
+  cin >> rg;
+  if(size == 0){
+    addinit(pointer, &size, nome, rg);
+  }else{
+    addend(pointer, &size, nome, rg);
+  }
+ }
+}
  return 0;
 }
