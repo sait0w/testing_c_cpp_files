1fb340eb2ac0311373037d71ed1323b65ff7ca57
Pedro Saito <phenriquesaito@gmail.com>
2022-08-26 19:39:46 -0300

    check_later

--- chained_list.cpp
+++ chained_list.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <string.h>
 #include <stdlib.h>
-
 using namespace std; 
 
 struct estrutura{
@@ -12,7 +11,10 @@ struct estrutura{
 };
 
 int main(){
- estrutura *chainpt;
-
+ estrutura algo = {13, NULL};
+ estrutura *chainpt;  //Ponteiro encadeado.
+ estrutura *onel = new estrutura;  //Primeiro valor.       
+ 
+ 
  return 0;
 }
--- smll_tst.cpp
+++ smll_tst.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <new>
 #include <string>
+#include <string.h>
 #include <stdlib.h>
 using namespace std; 
 
-typedef struct food{
+struct food{
  string cor;
- struct food banana;
-}fruits;
+ struct food *banana;  //Só funciona com ponteiro?
+};
 
 int main(){
- 
+ food *pt = new food;
+
+ pt->banana = NULL;
+ pt->cor = "Amarela";
  return 0;
 }
