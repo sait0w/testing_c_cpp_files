b1ac7eb2ab183e3c0c13d1bd0f542ac1d4e9abb1
Pedro Saito <phenriquesaito@gmail.com>
2022-09-02 19:17:16 -0300

    proj

--- proj_/jgforca.cpp
+++ proj_/jgforca.cpp
@@ -12,6 +12,7 @@ void cls();
  int main(){
  setlocale(LC_ALL,"Portuguese");
  char iput;
+ int res = 0;
 
  do{
  cls();
@@ -23,25 +24,30 @@ void cls();
  print_dash(72);
  cout << endl;
  space(20);
- cout << "Bem-Vindo ao Jogo da Forca™!! ˆˆ\n" << endl;
+ cout << "Bem-Vindo ao Jogo da Forca™!! ˆˆ\n";
  space(30);
  cout << "INSTRUÇÕES:\n" << endl;
  cout << "-> Será disponibilizada uma dica sobre a palavra.\n";
  cout << "-> O número de tentativas é número de letras + 2.\n";
- cout << "-> A mensagem \x1b[32m'correto'\x1b[0m aparecerá se acertar letra.\n";
- cout << "-> A mensagem \x1b[31m'incorreto'\x1b[0m aparecerá se errar letra.\n" << endl;
+ cout << "-> A mensagem \x1b[32m'correto'\x1b[0m aparecerá se acertar letra.\n";   //Alterei a cor default do txt
+ cout << "-> A mensagem \x1b[31m'incorreto'\x1b[0m aparecerá se errar letra.\n";
  print_menu2();
  cout << "-> Aperte '1' se deseja prosseguir.\n";
  cin >> iput;
-  if(iput == 'e'){
+  if(iput == 'e'){     //Comando 'e' p/sair do jogo
     cls();
     return 1;
    }
   }while(iput == '0');
  cls();
-
-
-
+ cout << endl;
+ do{
+ print_forca();
+ cout << endl;
+ 
+ cin >> res;
+ cls();
+ }while(res == 0);
  return 0;
  }
 
